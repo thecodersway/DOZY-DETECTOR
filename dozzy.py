0ec36dc3a4f3bfb2ab5bc1416d9a4624a41dfb9f
thecodersway <125991240+thecodersway@users.noreply.github.com>
2024-06-10 14:47:09 +0530

    Update dozzy.py

--- dozzy.py
+++ dozzy.py
@@ -1,1 +1,74 @@
-print("hello")
+from scipy.spatial import distance
+from imutils import face_utils
+from pygame import mixer
+import imutils
+import dlib
+import cv2
+
+
+mixer.init()
+mixer.music.load("C:/Users/hp/OneDrive/Desktop/Driver_Drowsiness/music.wav")
+
+def eye_aspect_ratio(eye):
+	A = distance.euclidean(eye[1], eye[5])
+	B = distance.euclidean(eye[2], eye[4])
+	C = distance.euclidean(eye[0], eye[3])
+	ear = (A + B) / (2.0 * C)
+	return ear
+
+def mouth_aspect_ratio(mouth):
+	A = distance.euclidean(mouth[0], mouth[5])
+	B = distance.euclidean(mouth[1], mouth[8])
+	C = distance.euclidean(mouth[2], mouth[7])
+	D = distance.euclidean(mouth[3], mouth[6])
+	mar = (B + C + D) / (2.0 * A)
+	return mar
+
+thresh = 0.25
+mar_thresh = 0.68
+frame_check = 20
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor("C:/Users/hp/OneDrive/Desktop/Driver_Drowsiness/shape_predictor_68_face_landmarks.dat")
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["mouth"]
+cap=cv2.VideoCapture(0)
+flag=0
+while True:
+	ret, frame=cap.read()
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	subjects = detect(gray, 0)
+	for subject in subjects:
+		shape = predict(gray, subject)
+		shape = face_utils.shape_to_np(shape)
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		mouth = shape[mStart:mEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+		ear = (leftEAR + rightEAR) / 2.0
+		mar = mouth_aspect_ratio(mouth)
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		mouthHull = cv2.convexHull(mouth)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+		if ear < thresh or mar < mar_thresh:
+			flag += 1
+			if flag >= frame_check:
+				cv2.putText(frame, "warning!", (10, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				cv2.putText(frame, "warning!", (10,325),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				mixer.music.play()
+		else:
+			flag = 0
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
+		break
+cv2.destroyAllWindows()
+cap.release()
